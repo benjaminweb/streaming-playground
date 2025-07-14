75787436e0a2e68f5e882e19364a092e2827e77c
Benjamin Weber <one@bwe.im>
2025-07-14 12:31:46 +0200

    implement streamRows as pendant to getRows

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,8 @@ import Streaming
 import Data.Char
 import Data.IORef
 import Control.Concurrent.Async
+import Data.Maybe (listToMaybe)
+import Data.Time.Clock.System
 
 tut1 :: IO (Of Int ())
 tut1 = S.sum $ S.take 3 (S.readLn :: Stream (Of Int) IO ())
@@ -58,6 +60,9 @@ simpleIO' = mapM_ S.yield [1..10]
 simpleIO2' :: (Monad m, Num a) => (a -> m b) -> m ()
 simpleIO2' f = mapM_ f [1,2,3,4,5,6,7,8,9,10]
 
+simpleIO3 :: (Monad m, Num a) => (a -> m b) -> m [b]
+simpleIO3 f = traverse f [1,2,3,4,5,6,7,8,9,10]
+
 -- possibly easiest first step: https://github.com/valderman/selda/blob/ab9619db13b93867d1a244441bb4de03d3e1dadb/selda/src/Database/Selda/Compile.hs#L153
 -- => create streaming variant of `nextResult`
 --    calls `gNextResult` https://github.com/valderman/selda/blob/ab9619db13b93867d1a244441bb4de03d3e1dadb/selda/src/Database/Selda/SqlRow.hs#L58
@@ -69,6 +74,93 @@ simpleIO2' f = mapM_ f [1,2,3,4,5,6,7,8,9,10]
 -- unclear:
 --   o how does (:*:) work? and how does it can / need to be used with Streaming return types (if it's resembling a list type)?
 --   o to be ctd.
+
+-- grasp some basic streaming building blocks (we are abstracting the streaming library's specifics away)
+
+single :: (Monad m, Num a) => (a -> m b) -> a -> m b
+single f x = f x
+
+-- >>>  S.sum $ two S.yield 22 23
+-- 45 :> ()
+two :: (Monad m, Num a, Monoid (m b)) => (a -> m b) -> a -> a -> m b
+two f x y = f x `mappend` f y
+
+-- >>> S.sum $ anyNo [22, 23]
+-- 45 :> ()
+anyNo :: (Monad m, Num a, Monoid (m b)) => (a -> m b) -> [a] -> m b
+anyNo f (a:xs) = foldl (\acc x -> acc `mappend` f x) (f a) xs
+anyNo _ [] = mempty
+
+
+{- let's take the function we want to streamify
+getRows :: Statement -> [[SQLData]] -> IO [[SQLData]]
+getRows s acc = do
+  res <- step s
+  case res of
+    Row -> do
+      cs <- columns s :: IO [SQLData]
+      getRows s (acc ++ [cs])
+    _ -> do
+      return $ acc
+-}
+
+-- | Let's rebuild it as a toy to play with…
+-- 
+-- >>> getRowsNaive ((take 22 $ repeat False) ++ [True]) []
+-- [1752481033,1752481033,1752481033,1752481033,1752481033,1752481033,1752481033,1752481033,1752481033,1752481033,1752481033,1752481033,1752481033,1752481033,1752481033,1752481033,1752481033,1752481033,1752481033,1752481033,1752481033,1752481033]
+getRowsNaive :: [Bool] -> [Int] -> IO [Int]
+getRowsNaive yetDone acc = do
+  case listToMaybe yetDone of
+    Just True -> return acc
+    _ -> do
+        t <- fromIntegral . systemSeconds <$> getSystemTime
+        getRowsNaive (drop 1 yetDone) (acc ++ [t])
+
+-- | How could we stream just but a single element?
+-- 
+-- >>> S.print $ streamSingle S.yield
+-- 1752483198
+streamSingle :: (MonadIO m, Monad m, Num a, Monoid (m b)) => (a -> m b) -> (m b)
+streamSingle f = do
+  t <- liftIO $ fromIntegral . systemSeconds <$> getSystemTime
+  f t
+
+-- | How could we add a single element to an existing stream?
+-- 
+-- >>> S.sum $ streamNext False S.yield (S.yield (-99999999999))
+-- -98247516591 :> ()
+streamNext :: (MonadIO m, Monad m, Num a , Monoid (m b)) => Bool -> (a -> m b) -> m b -> m b
+streamNext False f acc = do
+  t <- liftIO $ fromIntegral . systemSeconds <$> getSystemTime
+  acc `mappend` f t
+streamNext True _ acc = acc
+
+-- | How could we turn `getRowsNaive` into stream variant?
+-- 
+-- >>> S.toList $ streamRowsNaive S.yield ((take 22 $ repeat False) ++ [True]) mempty
+-- [1752483783,1752483783,1752483783,1752483783,1752483783,1752483783,1752483783,1752483783,1752483783,1752483783,1752483783,1752483783,1752483783,1752483783,1752483783,1752483783,1752483783,1752483783,1752483783,1752483783,1752483783,1752483783] :> ()
+streamRowsNaive :: (MonadIO m, Monad m, Num a, Monoid (m b)) => (a -> m b) -> [Bool] -> m b -> m b
+streamRowsNaive f yetDone acc = do
+  case listToMaybe yetDone of
+    Just True -> acc
+    _ -> do
+        t <- liftIO $ fromIntegral . systemSeconds <$> getSystemTime
+        streamRowsNaive f (drop 1 yetDone) (acc `mappend` f t)
+
+-- | How do we transfer this to the original getRows variant?
+-- 
+-- >>> streamRows S.yield "foo bar" mempty
+-- TBD
+{-
+streamRows :: (MonadIO m, Monad m, Monoid (m b)) => (a -> m b) -> Statement -> m b -> m b
+streamRows f s acc = do
+  res <- step s
+  case res of
+    Row -> do
+      cs <- liftIO (columns s :: IO [SQLData])
+      streamRows f s (acc `mappend` f cs)
+    _ -> acc
+-}
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
